71d8965a88dcb0518015cf91f2f14d5951c13f8e
NimishRathi <nimish.rathi2002@gmail.com>
2025-01-08 16:17:15 +0530

    new codes

---
+++ SimpleInterest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class SimpleInterest{
+public static void main(String []args){
+Scanner sc = new Scanner(System.in);
+int Principal = sc.nextInt();
+int Rate = sc.nextInt();
+int Time = sc.nextInt();
+double SimpleInterest =(Principal * Rate * Time) / 100;
+System.out.println(SimpleInterest);
+}
+}	
