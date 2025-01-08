a64acac6a2c697183b66615afea9e641d6229315
NimishRathi <nimish.rathi2002@gmail.com>
2025-01-08 12:00:53 +0530

    done

--- welcome.java
+++ welcome.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class welcome {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("welcome to java ");
+        System.out.print("welcome to java");
         
     }
 }
